35228028858b5ac9b3604f613f94ff2a1d04af84
MasFuerte <two_21@mail.ru>
2024-06-13 20:55:03 +0300

    added check_json_value

--- utils/checking.py
+++ utils/checking.py
@@ -8,8 +8,14 @@ class Checking:
         assert status_code == response.status_code, "Ошибка!"
 
     @staticmethod
-    def check_json_token(response, expected_value):
+    def check_json_keys(response, expected_value):
         """Метод для проверки обязательных полей"""
         token = json.loads(response.text)
         assert list(token) == expected_value, "Ошибка!"
 
+    @staticmethod
+    def check_json_value(response, field_name, expected_value):
+        """Метод для проверки значений обязательных полей в ответе запроса"""
+        check = response.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value, "Ошибка!"
